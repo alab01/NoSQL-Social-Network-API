535893208f3720fa7107aa8f4daafa8e342a7f77
Alan Abed <alanabed@Alans-MacBook-Air.local>
2022-07-26 15:48:46 -0700

    completed most of the models

--- models/Users.js
+++ models/Users.js
@@ -1,23 +1,41 @@
 const { Schema, model } = require('mongoose');
 
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 const userSchema = new Schema(
     {
         username: {
             type: String,
             required: true,
-
+            unique: true,
+            trim: true
         },
         email: {
             type: String,
             required: true,
-
+            unique: true,
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
         },
-        thoughts: {
-
-        },
-        friends: {
-            
-        }
-
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Users',
+            },
+        ],
     }
 )
+
+const User = model('user', userSchema);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const Users = require('./Users');
+const Reaction = require('./Reaction');
+const Thought = require('./Thought');
+
+module.exports = { Users, Reaction, Thought };
